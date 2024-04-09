293eebd28c3223a0e67b87fe0700f41cdaad0353
= <=>
2024-04-09 08:32:52 -0400

    chore: change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,49 +1,12 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000
- },
-  -- {
-  --   "ribru17/bamboo.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("bamboo").setup({
-  --       -- optional configuration here
-  --     })
-  --     require("bamboo").load()
-  --   end,
-  -- },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --     styles = {
-  --       sidebars = "transparent",
-  --       floats = "transparent",
-  --     },
-  --     dim_inactive = true,
-  --     lualine_bold = false,
-  --     hide_inactive_statusline = false,
-  --   },
-  -- },
-}
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
 
--- Embark Theme
--- return {
---   {
---     "embark-theme/vim",
---   },
--- }
---
--- Synthweave
--- return {
---   {
---     "samharju/synthweave.nvim",
---     lazy = false, -- make sure we load this during startup if it is your main colorscheme
---     priority = 1000,
---     config = function()
---       vim.cmd.colorscheme("synthweave-transparent")
---     end,
---   },
--- }
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
