d93073cebbcd753583818a34b95041622fc8fced
= <=>
2024-04-24 09:53:51 -0400

    chore: more config updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,20 @@
 
 local keymap = vim.keymap
 
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   pattern = "*",
+--   callback = function()
+--     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0) == "" then
+--       vim.cmd("Neotree reveal toggle")
+--     end
+--   end,
+-- })
+
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+
+-- Keybinding to toggle Neo-tree
+vim.keymap.set("n", "<C-n>", ":Neotree reveal toggle<CR>", { noremap = true, silent = true })
 vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<C-CR>", "i<CR><Esc>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
