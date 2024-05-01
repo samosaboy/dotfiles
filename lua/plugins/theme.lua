fb82ca3fafed4048aa16887fa94065c78bdb23b8
= <=>
2024-05-01 11:21:05 -0400

    chore: add monet

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,45 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
+  {
+    "fynnfluegge/monet.nvim",
+    name = "monet",
+  },
+  -- {
+  --   "scottmckendry/cyberdream.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("cyberdream").setup({
+  --       transparent = true, -- Default: false
+  --       italic_comments = true, -- Default: false
+  --       hide_fillchars = true, -- Default: false
+  --       borderless_telescope = true, -- Default: true
+  --       terminal_colors = true, -- Default: true
+  --       -- theme = { -- Default: nil
+  --       --   highlights = {
+  --       --     Comment = { fg = "#696969", bg = "NONE", italic = true },
+  --       --   },
+  --       --
+  --       --   -- Override a color entirely
+  --       --   colors = {
+  --       --     -- For a list of colors see `lua/cyberdream/colours.lua`
+  --       --     -- Example:
+  --       --     bg = "#000000",
+  --       --     green = "#00ff00",
+  --       --     magenta = "#ff00ff",
+  --       --   },
+  --       -- },
+  --     })
+  --     vim.cmd("colorscheme cyberdream") -- set the colorscheme
+  --   end,
+  -- },
+  -- -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  --
+  -- -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "monet",
     },
   },
 }
