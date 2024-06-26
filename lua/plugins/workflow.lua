33106cc206301b5d8ee478c8a9ee0f38b8723523
= <=>
2024-06-26 15:38:35 -0400

    chore: save config changes again

--- lua/plugins/workflow.lua
+++ lua/plugins/workflow.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {
-      -- allow_different_key = true,
-      -- hints = {
-      --   ["[dcyvV][ia][%(%)]"] = {
-      --     message = function(keys)
-      --       return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
-      --     end,
-      --     length = 3,
-      --   },
-      -- },
-    },
-  },
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  --   opts = {
+  --     -- hints = {
+  --     -- allow_different_key = true,
+  --     --   ["[dcyvV][ia][%(%)]"] = {
+  --     --     message = function(keys)
+  --     --       return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
+  --     --     end,
+  --     --     length = 3,
+  --     --   },
+  --     -- },
+  --   },
+  -- },
   -- {
   --   "fedepujol/move.nvim",
   --   keys = {
